57a6a732281f05f71ce75baaea08c8f81dc11419
kiplmoboor <kiplimoarapboor@gmail.com>
2023-07-03 21:28:10 +0300

    adds node to end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,10 +15,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new = malloc(sizeof(listint_t));
 
-	if(!new)
+	if (!new)
 	{
 		printf("Error!\n");
-		return NULL;
+		return (NULL);
 	}
 
 	new->n = n;
