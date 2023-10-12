47091becf525ac0900a48f93db7ae5f152bc09ea
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-12 21:20:45 +0000

    deletes node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!(*head))
 		return (-1);
-  
+
 	if (index == 0)
 	{
 		if (temp->next)
@@ -26,14 +26,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(*head) = temp->next;
 		return (1);
 	}
-	
+
 	while (temp)
 	{
 		if (i == index)
 		{
 			if (temp->next)
 				temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
+			if (temp->prev)
+				temp->prev->next = temp->next;
 			return (1);
 		}
 		i++;
