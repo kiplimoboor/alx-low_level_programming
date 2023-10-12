638add04e3248ca21247dec1f763a66481778715
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-12 20:17:16 +0000

    deletes the node at index index of a dlistint_t linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes node at a specific position
+ * in a doubly linked list
+ *
+ * @head: the doubly liked list head
+ * @index: the position to be deleted
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
+
+	if (!(*head))
+		return (-1);
+
+	if (index == 0)
+	{
+		(*head) = (*head)->next;
+		return (1);
+	}
+
+	while (temp)
+	{
+		if (i == index)
+		{
+			if (temp->next)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			return (1);
+		}
+		i++;
+		temp = temp->next;
+	}
+
+	if (i < index)
+		return (-1);
+
+	return (1);
+}
