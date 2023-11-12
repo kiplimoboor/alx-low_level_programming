8d83d699ba63a83fb5c90cf1cb8fa6e97bba5e9f
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-12 21:55:55 +0300

    retrieves value of key in hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get- retrieves a value from hash table associated with a key
+ *
+ * @ht: the hash table
+ * @key: key being looked at
+ *
+ * Return: value associated with key, (NULL) otherwise
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	if (node == NULL)
+		return (NULL);
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
