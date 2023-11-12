4110e1f6adb473cb2044c16471ed6488e625e08b
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-12 21:09:16 +0300

    sets value in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,4 +12,39 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *item = malloc(sizeof(hash_node_t));
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+	unsigned long int index;
+
+	if (node == NULL || item == NULL)
+		return (0);
+
+	item->key = strdup(key);
+	item->value = strdup(value);
+	item->next = NULL;
+
+	index = key_index((const unsigned char*)key, ht->size);
+
+	node = ht->array[index];
+
+	if (node == NULL)
+	{
+		ht->array[index] = item;
+		return (1);
+	}
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
+	}
+
+	item->next = ht->array[index];
+	ht->array[index] = item;
+	return (1);
 }
