b2397dc25b2e53543d934091d66f5eacbcdd2c1e
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-12 17:34:49 +0300

    gives index of a key.

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index- gives index of key
+ *
+ * @key: the key
+ * @size: size of array of hash table
+ *
+ * Return: the index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
