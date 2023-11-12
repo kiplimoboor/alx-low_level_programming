1279177e0a9c9882bb528c01de7cf4aef3fed8e5
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-12 23:20:31 +0300

    deleted a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: the hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node;
+	hash_node_t *next_node;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+
+		while (current_node)
+		{
+			next_node = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			current_node = next_node;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
