04ac24ac99ba0e6b51933424906abaffa58d0b96
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-12 16:49:06 +0300

    creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table;
-
-	if (size <= 0)
-		return (NULL);
-
-	table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
