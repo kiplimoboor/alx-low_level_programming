5baf1640a7e7a34c2c775c4551a9978c1425efd9
kiplimoboor <kiplimoboor@pm.me>
2023-12-20 20:56:12 +0300

    perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    Module for finding perimeter
+"""
+
+
+def island_perimeter(grid):
+    """Finds the perimeter of an island
+
+    Args:
+        grid (list): list of list of integers:
+                     0 represents a water zone
+                     1 represents a land zone
+                     One cell is a square with side length 1
+                     Grid cells are connected horizontally/vertically.
+                     Grid is rectangular, width and height don't exceed 100
+
+    Returns:
+        int: the perimeter o the island
+    """
+
+    height = len(grid)
+    width = len(grid[0])
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
